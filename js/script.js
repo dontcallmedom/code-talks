27a66d6dbf17c9468e26f64fa14b97f701c54544
Dominique Hazael-Massieux <dom@w3.org>
2012-05-22 13:01:29 +0200

    now based on string match rather than line/col

--- js/script.js
+++ js/script.js
@@ -10,27 +10,43 @@ jQuery(document).ready(function ($) {
      playLine(lines[0]+"\n", function () { if (lines.length > 1) playLines(lines.slice(1));});
  }
  function playLine(line, next) {
+     function finishLine() {
+         cursor.line = played.length;
+	 cursor.col=0;
+	 played.push("");
+	 next();
+	 prettyPrint();	 
+     }
+
      if (line.length > 0) {
-	 if (line[0] == '#' || line[0]=='@') {
-	   var command = line[0];
-	   var comp = line.slice(1).split(":");
-	   var cur = comp[0].split(",");
-	   cursor.line = parseInt(cur[0],10);
-	   var col = parseInt(cur[1],10);
-	   console.log(col);
-	   if (col >= 0) {
-  	       cursor.col = col;
+	 if (line[0] == '#') {
+	   var command = line[1];
+	   var comp = line.slice(2).split("§");
+	   line = comp.slice(1).join("§");
+	   var search = comp[0];
+	   console.log(search);
+	   var text = played.join("");
+	   var match = text.indexOf(search);
+	   if (match === -1) {
+	       finishLine();
 	   } else {
-  	       cursor.col = played[cursor.line].length + col - 1;
-	   }
-	   console.log(cursor.col);
-	   line = comp.slice(1).join(":");
-	   if (command=='#') { // insert new line
-               var queue = played.slice(cursor.line) ;
-               played = played.slice(0,cursor.line).concat("",queue) ;	       
-	   } else if (command=='@') {
-	       // remove extraneous "\n"
-	       line = line.slice(0,-1);
+	       console.log(match);
+	       var head = text.slice(0,match);
+	       console.log(head);
+	       // Calculate line
+	       cursor.line = head.split("\n").length - 1;
+	       // Calculate column if appending
+	       if (command == "a") {
+		   cursor.col = match - head.lastIndexOf("\n") + search.length - 1;
+		   // remove extraneous "\n"
+		   line = line.slice(0,-1);
+	       } else if (command == "i") {
+		   console.log("Insert " + line + " before " + search + " at line " + cursor.line);
+		   cursor.col = 0;
+		   var queue = played.slice(cursor.line) ;
+		   played = played.slice(0,cursor.line).concat("",queue) ;	       
+	       }
+	       console.log(cursor);
 	   }
        }
        var newline = played[cursor.line].slice(0,cursor.col) +  line[0] + played[cursor.line].slice(cursor.col);
@@ -39,13 +55,9 @@ jQuery(document).ready(function ($) {
        play.text(played.join(""));
        cursor.col++;
        if (line.length > 1 ) {
-         setTimeout(function() { playLine(line.slice(1), next);}, 50);	 
+         setTimeout(function() { playLine(line.slice(1), next);}, 10);	 
        } else {
-         cursor.line = played.length;
-	 cursor.col=0;
-	 played.push("");
-	 next();
-	 prettyPrint();
+	   finishLine();
        }
      }
  }
